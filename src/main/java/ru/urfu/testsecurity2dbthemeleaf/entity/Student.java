474e9e7fc5b648ea36a922e5f2785b312467bc42
VaganovaIP <iamvaganovanz.com>
2022-10-29 23:02:18 +0500

    Лабораторная работа №7

---
+++ src/main/java/ru/urfu/testsecurity2dbthemeleaf/entity/Student.java
@@ -0,0 +1,29 @@
+package ru.urfu.testsecurity2dbthemeleaf.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "STUDENTS")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "faculty")
+    private String faculty;
+    @Column(name = "age")
+    private int age;
+}
